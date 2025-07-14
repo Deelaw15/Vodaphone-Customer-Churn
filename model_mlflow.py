b14236b12e676ab2d41df8ce8a30e01abfc51f8f
Deelaw15 <lawalfaruq50@gmail.com>
2025-07-14 22:20:13 +0100

    new

--- model_mlflow.py
+++ model_mlflow.py
@@ -6,13 +6,13 @@ import joblib
 # Load the pre-trained model
 model = joblib.load("vodafone_churn_model.joblib")
 
-max_iter = 1000
+accuracy = 0.99
 
 # Start MLflow tracking
 with mlflow.start_run():
     mlflow.log_param("model_type", "LogisticRegression")
     mlflow.log_param("source", "Pre-trained")
-    mlflow.log_param("max_iter", max_iter)
+    mlflow.log_param("max_iter", 1000)
     mlflow.log_metric("accuracy", accuracy)
 
     # Log the model artifact
